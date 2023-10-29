c292a1949fb0b0d7130b5e3b5f1cb0661e9a4e51
juniorDeveloper8 <be6323787@gmail.com>
2023-10-28 19:31:53 -0500

    subo proyecto

---
+++ README.md
@@ -0,0 +1,16 @@
+# Calculadora con Xamari app movil
+
+Descripción
+
+Aplicación movil con visual studio y el framework xamari
+esta aplicacion se maneja xml paralas vistas y c# para la parte logica.
+
+## Requisitos 
+
+- [visual Studio](https://visualstudio.microsoft.com/es/)
+- instalar xamari forms
+- andorid studio para el emulador más fluido o puede compilarse desde el mismo telefono OPCIONAL
+
+## Ejecución
+
+![compilacion](/doc/calcu.png)
---
+++ zrcalculadora/MainPage.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace zrcalculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private decimal primerNumero;
+        private string nombreOperador;
+        private bool isOperatorClicked; 
+
+        // para q los nuemros se muestren en el panel
+       /* private void BtnOne_Clicked(object sender, EventArgs e)
+        {
+            ZrcResult.Text ="1";
+        }
+       */
+
+        //para poner todos los numeros en el panel
+        private void BtnCommmon_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (ZrcResult.Text == "0" || isOperatorClicked)
+            {
+                isOperatorClicked = false;
+                ZrcResult.Text = button.Text;
+            }
+            else 
+            {
+                ZrcResult.Text += button.Text;
+            }
+        }
+        //boton c para limpiar panel
+        private void BtnClear_Clicked(object sender, EventArgs e)
+        {
+            ZrcResult.Text="0";
+        }
+        //boton x borra un numero a la izquierda por si nos pasamos
+        private void BtnX_Clicked(object sender, EventArgs e)
+        {
+            string number = ZrcResult.Text;
+
+            if (number != "0") {
+            
+                number =number.Remove(number.Length - 1, 1);  
+                
+                if (string.IsNullOrEmpty(number)) {
+                    
+                    ZrcResult.Text = "0";
+                }
+                else
+                {
+                    ZrcResult.Text = number;
+                }
+            }
+        }
+
+
+        //boton de operacion
+
+        private void BtnOperacion_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            isOperatorClicked = true;
+            nombreOperador = button.Text;
+            primerNumero = Convert.ToDecimal(ZrcResult.Text);
+        }
+
+        // boton de porcentaje
+        private async void BtnPorcentaje_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string number = ZrcResult.Text; 
+                if (number != "0") {
+
+                    decimal valorPorcentaje = Convert.ToDecimal(number);
+                    string result = (valorPorcentaje / 100).ToString("0.##");
+                    ZrcResult.Text = result;
+                }
+            }
+            catch(Exception ex) {
+                await DisplayAlert("Error",ex.Message,"ok");
+            }
+
+        }
+
+        // boton igual para resultados
+        private void BtnIgual_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal segundoNumero = Convert.ToDecimal(ZrcResult.Text);
+                string finalResult = Calcular(primerNumero, segundoNumero).ToString("0.##");
+                ZrcResult.Text = finalResult;
+            }
+            catch (Exception ex) {
+                DisplayAlert("Error", ex.Message, "ok");
+            }
+        }
+
+        // operaciones decimales
+
+        public decimal Calcular(decimal primerNumero, decimal segundoNumero)
+        {
+            decimal result = 0;
+            if (nombreOperador == "+")
+            {
+                result = primerNumero + segundoNumero;
+            } 
+            else if (nombreOperador == "-")
+            {
+                result = primerNumero - segundoNumero;
+            } 
+            else if (nombreOperador == "*")
+            {
+                result = primerNumero * segundoNumero;
+            }
+            else if (nombreOperador == "/")
+            {
+                result = primerNumero / segundoNumero;
+            }
+            return result;
+        }
+    }
+}
